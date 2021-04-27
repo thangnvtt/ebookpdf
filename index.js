1affd738ca55f7898cf86b8c24fb6218894d2ebe
thangnv6 <thangnv@foobla.com>
2021-04-28 01:26:00 +0700

    update cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 const port = process.env.PORT || 3000
 let progress = 0
 
-app.use(cors({ origin: "http://localhost:3000" }))
+app.use(cors({ origin: true, credentials: true }))
 
 app.get('/', (req, res) => {
     res.send('Hi! ^_^')
