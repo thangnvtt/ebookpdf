8d88b80bb31f277a25feead68c227d48ddc0d31c
thangnv <thangnv@foobla.com>
2021-04-27 17:11:55 +0700

    remove frontend

--- frontend/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/css/css.css'
-import './assets/css/jquery-ui-1.10.3.custom.min.css'
-import './assets/css/main.css'
-
-createApp(App).mount('#app')
