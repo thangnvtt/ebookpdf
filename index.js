110f93210a9a63382e423319fed163fe0e998e52
thangnv <thangnv@foobla.com>
2021-04-28 09:27:05 +0700

    set cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-
+const bodyParser = require('body-parser')
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
@@ -7,24 +7,10 @@ const app = express()
 const port = process.env.PORT || 3000
 let progress = 0
 
-app.use(function (req, res, next) {
+app.use(cors()) // <---- use cors middleware
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*')
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true)
-
-    // Pass to next layer of middleware
-    next()
-})
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hi! ^_^')
